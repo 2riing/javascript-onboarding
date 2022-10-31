a1f11ea7f29b3e9dec0846ecc60230e63c8332bc
2riing <5032303000@naver.com>
2022-10-31 23:52:22 +0900

    problem5: 돈과 빈 0배열 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,4 +1,9 @@
+// greedy 문제
+
 function problem5(money) {
+  const moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let bucket = new Array(9).fill(0);
+  console.log(bucket);
   var answer;
   return answer;
 }
