ba123b99d2cd277c08e910b616d147a6b717f7fd
2riing <5032303000@naver.com>
2022-10-31 23:48:24 +0900

    problem4: 주석추가

--- src/problem4.js
+++ src/problem4.js
@@ -3,8 +3,10 @@ function problem4(word) {
   for (let value of word) {
     const num = value.charCodeAt(0);
     if (65 <= num && num <= 90) {
+      // 알파벳 대문자일 경우
       answer += String.fromCharCode(155 - num);
     } else if (97 <= num && num <= 122) {
+      // 알파벳 소문자일 경우
       answer += String.fromCharCode(219 - num);
     } else answer += value;
   }
