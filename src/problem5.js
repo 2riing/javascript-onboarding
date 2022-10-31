7d08b0337d33214eeb0507b08d1d4cb4c5689598
2riing <5032303000@naver.com>
2022-10-31 23:56:48 +0900

    problem5: 그리디 로직

--- src/problem5.js
+++ src/problem5.js
@@ -3,11 +3,13 @@
 function problem5(money) {
   const wons = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let bucket = new Array(9).fill(0);
-  wons.map((won) => {
+  const answer = wons.map((won) => {
     const cnt = Math.floor(money / won);
+    money -= cnt * won;
+    return cnt;
   });
   console.log(bucket);
-  var answer;
+
   return answer;
 }
 
