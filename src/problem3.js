2b5bb7c85ebae8669243b0d21103dba4d8bdf5c6
2riing <5032303000@naver.com>
2022-10-31 23:29:33 +0900

    problem3: 디버깅 코드 삭제

--- src/problem3.js
+++ src/problem3.js
@@ -2,7 +2,6 @@ function problem3(number) {
   let answer = 0;
   for (let i = 1; i <= number; i++) {
     let num = i + "";
-    console.log(num);
     for (let j = 0; j < num.length; j++) {
       if (num[j] == 3 || num[j] == 6 || num[j] == 9) {
         answer += 1;
