d7b7f375259152a66a694d6344dc88da28d4d94c
2riing <5032303000@naver.com>
2022-10-31 23:28:25 +0900

    problem3: number 범위 변경

--- src/problem3.js
+++ src/problem3.js
@@ -1,8 +1,8 @@
 function problem3(number) {
-  let answer;
-
-  for (let i = 1; i < number + 1; i++) {
+  let answer = 0;
+  for (let i = 1; i <= number; i++) {
     let num = i + "";
+    console.log(num);
     for (let j = 0; j < num.length; j++) {
       if (num[j] == 3 || num[j] == 6 || num[j] == 9) {
         answer += 1;
