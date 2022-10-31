a34fee341251e5650adde4c4575c51603ed95293
2riing <5032303000@naver.com>
2022-10-31 23:54:48 +0900

    problem5: 변수명 변경 moneys => wons

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,11 @@
 // greedy 문제
 
 function problem5(money) {
-  const moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const wons = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let bucket = new Array(9).fill(0);
+  wons.map((won) => {
+    const cnt = Math.floor(money / won);
+  });
   console.log(bucket);
   var answer;
   return answer;
