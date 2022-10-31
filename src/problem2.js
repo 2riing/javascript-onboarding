7de59cc17a51797e128949e6408ff8f31cff4c89
2riing <5032303000@naver.com>
2022-10-31 23:10:09 +0900

    problem2 : 문제 풀이

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
+  let stack = [cryptogram[0]];
 
+  for (let i = 1; i < cryptogram.length; i++) {
+    // 마지막 요소와 현재 추가할 요소가 같으면 pop
+    if (stack.slice(-1)[0] == cryptogram[i]) {
+      stack.pop();
+    } else {
+      stack = [...stack, cryptogram[i]];
+    }
+  }
+
+  return stack.join("");
+}
 module.exports = problem2;
