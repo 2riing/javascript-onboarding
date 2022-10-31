899c480dd45df86fb8d6b8352fac691dfcd387cb
2riing <5032303000@naver.com>
2022-11-01 00:11:11 +0900

    problem6: patterns set create

--- src/problem6.js
+++ src/problem6.js
@@ -1,7 +1,11 @@
 function problem6(forms) {
   var answer;
+  const Patterns = {};
 
   forms.forEach(([email, nickname]) => {
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const pattern = nickname.substring(i, i + 2);
+    }
     console.log(email, nickname);
   });
 
