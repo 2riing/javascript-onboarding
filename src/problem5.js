8ce2d7ba27819f8bb9cb1b71f8421fe6e1055329
2riing <5032303000@naver.com>
2022-10-31 23:57:24 +0900

    problem5: 주석 제거

--- src/problem5.js
+++ src/problem5.js
@@ -2,14 +2,11 @@
 
 function problem5(money) {
   const wons = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  let bucket = new Array(9).fill(0);
   const answer = wons.map((won) => {
     const cnt = Math.floor(money / won);
     money -= cnt * won;
     return cnt;
   });
-  console.log(bucket);
-
   return answer;
 }
 
